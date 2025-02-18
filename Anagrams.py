2938f951355a194e5ba48d2ebb7eacd381717cb6
Felipe Gonçalves de Lima <felipe.gdelima10@gmail.com>
2025-02-17 21:25:11 -0300

    Update Anagrams.py

--- Anagrams.py
+++ Anagrams.py
@@ -23,24 +23,40 @@ Example 3:
 Input: strs = ["a"]
 
 Output: [["a"]]
+
+QueijoRalado falando, esse exercício foi um inferno na terra para terminar (não terminei ainda, apenas escrevei que "foi" pois eu vou terminar isso aqui na base do ódio)
 '''
 
 coisax = ["eat","tea","tan","ate","nat","bat"]
+coisax2 = coisax.copy()
+
 anagramas_falso = []
 anagramas = []
 anagramas_definitivo = []
 checar_escolhida = sorted(coisax[0])
+checagem = 0
+cheio = False
+
 
 n2=0
 while len(coisax)!=0:
     n2=0
     palavra_escolhida = sorted(coisax[0])
-    for n in range (len(coisax)):
-        while n2!=len(coisax):
-            if palavra_escolhida==sorted(coisax[n2]):
-                anagramas_falso.append(coisax[n2])
-                coisax.pop(n2)
-                n2-=1
-            n2+=1
-anagramas_definitivo.append(anagramas_falso)
-print(anagramas_definitivo)
+    print("palavra escolhida", palavra_escolhida)
+    while n2!=len(coisax):
+        print("n2", n2)
+        if palavra_escolhida==sorted(coisax[n2]):
+            anagramas_falso.append(coisax[n2])
+            coisax.pop(n2)
+            n2-=1
+            print("falso", anagramas_falso)
+        n2+=1
+        if (n2==len(coisax)):
+            cheio=True
+
+    if cheio:
+        anagramas_definitivo.append(anagramas_falso)
+    print("definitivo 1", anagramas_definitivo)
+    while len(anagramas_falso)!=0:
+        anagramas_falso.pop()
+    print("definitivo 2", anagramas_definitivo)
